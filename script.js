8821fa2d750b5fb1ab4f1c709c58cc700538ba19
knight-dale <edwardsjonathan457@gmail.com>
2026-05-12 13:43:00 +0300

    update

--- script.js
+++ script.js
@@ -54,6 +54,21 @@ if (contactForm && submitBtn) {
   });
 }
 
+let lastScrollTop = 0;
+const navbar = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add('nav-hidden');
+  } else {
+    navbar.classList.remove('nav-hidden');
+  }
+  
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
+
 const projectData = {
   'CYSWEP': {
     title: "County Youth Service & Women Empowerment Program",
